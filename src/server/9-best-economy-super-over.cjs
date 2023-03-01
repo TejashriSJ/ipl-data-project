b938acc2c4c868903f35c2768e0edd7f2c33667d
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-03-01 14:00:40 +0530

    Add 9-best-economy-super-over file with superOverBestEconomy method
    This will return the bowler with best economy in super overs

---
+++ src/server/9-best-economy-super-over.cjs
@@ -0,0 +1,60 @@
+const csvtojson = require("./../node_modules/csvtojson");
+const fs = require("fs");
+
+function superOverBestEconomy(deliveries) {
+  if (deliveries === undefined || typeof deliveries !== "object") {
+    throw new Error("Parameters passed is not correct");
+  }
+  let superOverData = {};
+  let superOverEconomy = [];
+  //for each delivery if it is super over then take their data and count
+  deliveries.forEach((delivery) => {
+    if (delivery.is_super_over === "1") {
+      let runs = Number(delivery.total_runs);
+      let balls = 1;
+      let byeRuns = Number(delivery.bye_runs);
+      let legbyeRuns = Number(delivery.legbye_runs);
+
+      if (superOverData[delivery.bowler] === undefined) {
+        superOverData[delivery.bowler] = {
+          runs: runs,
+          balls: 1,
+          byeRuns: byeRuns,
+          legbyeRuns: legbyeRuns,
+        };
+      } else {
+        superOverData[delivery.bowler].runs += runs;
+        superOverData[delivery.bowler].balls += 1;
+        superOverData[delivery.bowler].byeRuns += byeRuns;
+        superOverData[delivery.bowler].legbyeRuns += legbyeRuns;
+      }
+    }
+  });
+  // calculate economy of each bowler in super overs
+  for (let bowler in superOverData) {
+    let runs = superOverData[bowler].runs;
+    let balls = superOverData[bowler].balls.toFixed(2);
+    let byeRuns = superOverData[bowler].byeRuns;
+    let legbyeRuns = superOverData[bowler].legbyeRuns;
+    let economyRate = ((runs - byeRuns - legbyeRuns) / (balls / 6)).toFixed(2);
+    superOverEconomy.push([bowler, economyRate]);
+  }
+  // best economy bowler
+  let bestEconomyBowler = superOverEconomy.sort((bowler1, bowler2) => {
+    return bowler1[1] - bowler2[1];
+  })[0];
+  return bestEconomyBowler;
+}
+
+csvtojson()
+  .fromFile("js-ipl-data-project/src/data/deliveries.csv")
+  .then((deliveriesObj) => {
+    let bestEconomyBowler = superOverBestEconomy(deliveriesObj);
+    fs.writeFileSync(
+      "js-ipl-data-project/src/public/output/9-best-economy-super-over.json",
+      JSON.stringify(bestEconomyBowler),
+      "utf-8"
+    );
+  });
+
+module.exports = superOverBestEconomy;
