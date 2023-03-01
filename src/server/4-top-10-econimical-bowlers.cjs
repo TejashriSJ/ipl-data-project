e2f00fdbe61599d047e490c6f8c51eac3189bd60
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-03-01 11:46:17 +0530

    Add 4-top-10-econimical-bowlers file with top10EconomicalBowlers method
    This will create an array of top 10 economical bowlers in 2015

---
+++ src/server/4-top-10-econimical-bowlers.cjs
@@ -0,0 +1,89 @@
+const csvtojson = require("./../node_modules/csvtojson");
+const fs = require("fs");
+
+function top10EconomicalBowlers(matches, deliveries) {
+  if (
+    matches === undefined ||
+    deliveries === undefined ||
+    typeof matches !== "object" ||
+    typeof deliveries !== "object"
+  ) {
+    throw new Error("Parameters passed is not correct");
+  }
+  let topEconomicalBowlers10 = [];
+  // taking ids from matches of 2015
+  const matchID2015 = matches
+    .filter((match) => {
+      return match.season === "2015";
+    })
+    .map((match) => {
+      return match.id;
+    });
+  // deliveries in only 2015
+  const deliveriesIn2015 = deliveries.filter((delivery) => {
+    return matchID2015.includes(delivery.match_id);
+  });
+
+  //creating each bowlers data containing their runs,balls,byeRuns,legbyRuns
+  const deliveryPerBowler = {};
+  deliveriesIn2015.forEach((delivery) => {
+    let bowler = delivery.bowler;
+    if (deliveryPerBowler[bowler] === undefined) {
+      deliveryPerBowler[bowler] = {
+        runs: Number(delivery.total_runs),
+        balls: 1,
+        byeRuns: Number(delivery.bye_runs),
+        legbyeRuns: Number(delivery.legbye_runs),
+      };
+    } else {
+      deliveryPerBowler[bowler].runs += Number(delivery.total_runs);
+      deliveryPerBowler[bowler].balls += 1;
+      deliveryPerBowler[bowler].byeRuns += Number(delivery.bye_runs);
+      deliveryPerBowler[bowler].legbyeRuns += Number(delivery.legbye_runs);
+    }
+  });
+
+  // calculating economy of each bowler
+  let economyOfBowlers = [];
+  for (bowler in deliveryPerBowler) {
+    let runs = deliveryPerBowler[bowler].runs;
+    let balls = deliveryPerBowler[bowler].balls.toFixed(2);
+    let byeRuns = deliveryPerBowler[bowler].byeRuns;
+    let legbyeRuns = deliveryPerBowler[bowler].legbyeRuns;
+    economyOfBowlers.push([
+      bowler,
+      (runs - byeRuns - legbyeRuns) / (balls / 6),
+    ]);
+  }
+  // sorting in ascending order of based on bowlers economy
+  economyOfBowlers.sort((bowler1, bowler2) => {
+    return bowler1[1] - bowler2[1];
+  });
+
+  // names of top 10 economical bowlers
+  topEconomicalBowlers10 = economyOfBowlers.slice(0, 10).map((bowlerRuns) => {
+    return bowlerRuns[0];
+  });
+  return topEconomicalBowlers10;
+}
+csvtojson()
+  .fromFile("js-ipl-data-project/src/data/matches.csv")
+  .then((matchObj) => {
+    csvtojson()
+      .fromFile("js-ipl-data-project/src/data/deliveries.csv")
+      .then((deliveriesObj) => {
+        let topEconomicalBowlers10;
+        topEconomicalBowlers10 = top10EconomicalBowlers(
+          matchObj,
+          deliveriesObj
+        );
+
+        fs.writeFileSync(
+          "js-ipl-data-project/src/public/output/4-top-10-econimical-bowlers.json",
+          JSON.stringify(topEconomicalBowlers10),
+          "utf-8"
+        );
+      });
+  });
+
+module.exports = top10EconomicalBowlers;
