e8a65ede5389f6a926681076a50ee162f7a8b1e0
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-03-01 11:29:39 +0530

    Add 3-extra-run-per-team-2016 file with extraRunsPerTeam method
    This will create an object containing extra runs per team for the given season

---
+++ src/server/3-extra-run-per-team-2016.cjs
@@ -0,0 +1,57 @@
+const csvtojson = require("./../node_modules/csvtojson");
+const fs = require("fs");
+
+function extraRunsPerTeam(matches, deliveries, extraRunPerTeam2016) {
+  if (
+    matches === undefined ||
+    deliveries === undefined ||
+    extraRunPerTeam2016 === undefined ||
+    typeof matches !== "object" ||
+    typeof deliveries !== "object"
+  ) {
+    throw new Error("Parameters passed is not correct");
+  }
+
+  // match ids in 2016
+  matchIDin2016 = matches
+    .filter((match) => {
+      return match.season === "2016";
+    })
+    .map((match) => {
+      return match.id;
+    });
+
+  for (let delivery of deliveries) {
+    // for each of the delivery in the given season counts extra runs per each team
+    if (matchIDin2016.includes(delivery.match_id)) {
+      if (extraRunPerTeam2016[delivery.bowling_team] === undefined) {
+        extraRunPerTeam2016[delivery.bowling_team] = Number(
+          delivery.extra_runs
+        );
+      } else {
+        extraRunPerTeam2016[delivery.bowling_team] += Number(
+          delivery.extra_runs
+        );
+      }
+    }
+  }
+}
+
+csvtojson()
+  .fromFile("js-ipl-data-project/src/data/matches.csv")
+  .then((matchObj) => {
+    csvtojson()
+      .fromFile("js-ipl-data-project/src/data/deliveries.csv")
+      .then((deliveriesObj) => {
+        const extraRunPerTeam2016 = {};
+        extraRunsPerTeam(matchObj, deliveriesObj, extraRunPerTeam2016);
+
+        fs.writeFileSync(
+          "js-ipl-data-project/src/public/output/3-extra-run-per-team-2016.json",
+          JSON.stringify(extraRunPerTeam2016),
+          "utf-8"
+        );
+      });
+  });
+
+module.exports = extraRunsPerTeam;
