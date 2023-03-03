266b211958c292a2943c653eed7b24b7552b5d0a
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-03-03 22:56:41 +0530

    Update 7-strike-rate-per-season file
    This will modify the strike rate by removing wide and no balls

--- src/server/7-strike-rate-per-season.cjs
+++ src/server/7-strike-rate-per-season.cjs
@@ -23,28 +23,45 @@ function strikeRateOfBatterPerSeason(matches, deliveries) {
     for (let delivery of deliveries) {
       if (match.id === delivery.match_id) {
         let batsman = delivery.batsman;
-
-        if (countRunsAndBalls[[batsman, season]] === undefined) {
-          countRunsAndBalls[[batsman, season]] = {
+        // wide and no ball will not be considered
+        let balls =
+          delivery.wide_runs === "0" && delivery.noball_runs === "0" ? 1 : 0;
+        if (countRunsAndBalls[batsman] === undefined) {
+          countRunsAndBalls[batsman] = {};
+        }
+        if (countRunsAndBalls[batsman][season] === undefined) {
+          countRunsAndBalls[batsman][season] = {
             runs: Number(delivery.batsman_runs),
-            balls: 1,
+            balls: balls,
           };
         } else {
-          countRunsAndBalls[[batsman, season]].runs += Number(
+          countRunsAndBalls[batsman][season].runs += Number(
             delivery.batsman_runs
           );
-          countRunsAndBalls[[batsman, season]].balls += 1;
+          countRunsAndBalls[batsman][season].balls += balls;
         }
       }
     }
   }
+  console.log(JSON.stringify(countRunsAndBalls));
+  //calculating strike of each batsman per season
+  Object.keys(countRunsAndBalls).forEach((batsman) => {
+    strikeRatePerSeason[batsman] = {};
+    Object.keys(countRunsAndBalls[batsman]).forEach((year) => {
+      strikeRatePerSeason[batsman][year] = {};
+      let runs = countRunsAndBalls[batsman][year]["runs"];
+      let balls = countRunsAndBalls[batsman][year]["balls"];
+      console.log(runs, balls);
+      countRunsAndBalls[batsman][year]["strike"] = (
+        (runs / balls) *
+        100
+      ).toFixed(2);
+      strikeRatePerSeason[batsman][year] = {
+        strike: countRunsAndBalls[batsman][year]["strike"],
+      };
+    });
+  });
 
-  // calculating strike of each batsman per season
-  for (let playerInSeason in countRunsAndBalls) {
-    let runs = countRunsAndBalls[playerInSeason].runs;
-    let balls = countRunsAndBalls[playerInSeason].balls;
-    strikeRatePerSeason[playerInSeason] = ((runs / balls) * 100).toFixed(2);
-  }
   return strikeRatePerSeason;
 }
 
